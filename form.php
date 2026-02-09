069c14b119a58d834b9089d065156d3e5f012f3e
Odoh Emmanuel <labotic8@gmail.com>
2026-02-09 04:14:41 -0800

    some forms working

--- form.php
+++ form.php
@@ -22,26 +22,26 @@
 <div class="af-element">
 <label class="previewLabel" for="awf_field-118411101-first">First Name:</label>
 <div class="af-textWrap">
-<input id="awf_field-118411101-first" type="text" class="text" name="first_name" value="" onfocus=" if (this.value == '') { this.value = ''; }" onblur="if (this.value == '') { this.value='';} " tabindex="501" />
+<input id="awf_field-118411101-first" type="text" class="text" name="First_name" value="" onfocus=" if (this.value == '') { this.value = ''; }" onblur="if (this.value == '') { this.value='';} " tabindex="501" />
 </div>
 <div class="af-clear"></div>
 </div>
 <div class="af-element">
 <label class="previewLabel" for="awf_field-118411101-last">Last Name:</label>
 <div class="af-textWrap">
-<input id="awf_field-118411101-last" class="text" type="text" name="last_name" value="" onfocus=" if (this.value == '') { this.value = ''; }" onblur="if (this.value == '') { this.value='';} " tabindex="502" />
+<input id="awf_field-118411101-last" class="text" type="text" name="Last_name" value="" onfocus=" if (this.value == '') { this.value = ''; }" onblur="if (this.value == '') { this.value='';} " tabindex="502" />
 </div>
 <div class="af-clear"></div>
 
 </div>
 <div class="af-element">
 <label class="previewLabel" for="awf_field-118411102">Email:</label>
-<div class="af-textWrap"><input class="text" id="awf_field-118411102" type="email" name="email" value="" tabindex="503" onfocus=" if (this.value == '') { this.value = ''; }" onblur="if (this.value == '') { this.value='';}" />
+<div class="af-textWrap"><input class="text" id="awf_field-118411102" type="Email" name="Email" value="" tabindex="503" onfocus=" if (this.value == '') { this.value = ''; }" onblur="if (this.value == '') { this.value='';}" />
 </div><div class="af-clear"></div>
 </div>
 <div class="af-element">
 <label class="previewLabel" for="awf_field-118411103">Phone</label>
-<div class="af-textWrap"><input type="text" id="awf_field-118411103" class="text" name="phone" value="" onfocus=" if (this.value == '') { this.value = ''; }" onblur="if (this.value == '') { this.value='';}" tabindex="504" /></div>
+<div class="af-textWrap"><input type="text" id="awf_field-118411103" class="text" name="Phone" value="" onfocus=" if (this.value == '') { this.value = ''; }" onblur="if (this.value == '') { this.value='';}" tabindex="504" /></div>
 <div class="af-clear"></div></div>
 
 <div class="af-element">
@@ -56,42 +56,42 @@
 
 <div class="af-element">
 <label class="previewLabel" for="awf_field-118411106">Instagram Page:</label>
-<div class="af-textWrap"><textarea rows="" cols="" name="Instagram_page " id="awf_field-118411106" onfocus="if (this.value == '') { this.value = ''; }" onblur="if (this.value == '') { this.value='';}" tabindex="507" style="height:60px"></textarea></div><div class="af-clear"></div></div>
+<div class="af-textWrap"><textarea rows="" cols="" name="instagram" id="awf_field-118411106" onfocus="if (this.value == '') { this.value = ''; }" onblur="if (this.value == '') { this.value='';}" tabindex="507" style="height:60px"></textarea></div><div class="af-clear"></div></div>
 
 <div class="af-element">
 <label class="previewLabel" for="awf_field-118411107">Product, Goods or Services You Sell:</label>
-<div class="af-textWrap"><textarea rows="" cols="" name=" Product " id="awf_field-118411107" onfocus="if (this.value == '') { this.value = ''; }" onblur="if (this.value == '') { this.value='';}" tabindex="508" style="height:60px"></textarea></div><div class="af-clear"></div></div>
+<div class="af-textWrap"><textarea rows="" cols="" name="products" id="awf_field-118411107" onfocus="if (this.value == '') { this.value = ''; }" onblur="if (this.value == '') { this.value='';}" tabindex="508" style="height:60px"></textarea></div><div class="af-clear"></div></div>
 
 <div class="af-element"><div><label class="previewLabel" for="awf_field-118411108-0">Target Audience: [check one]</label><div class="choiceList choiceList-radio-stacked">
 <div class="af-element-radio multiChoice">
-<input class="radio" type="radio" id="awf_field-118411108-0" name=" Audience"  value="Women" tabindex="509" />
+<input class="radio" type="radio" id="awf_field-118411108-0" name="audience"  value="Women" tabindex="509" />
 <label class="choice" for="awf_field-118411108-0">Women</label>
 
 <div class="af-clear"></div></div><div class="af-element-radio multiChoice">
-<input class="radio" type="radio" id="awf_field-118411108-1" name="custom Target Audience"  value="Men" tabindex="510" />
+<input class="radio" type="radio" id="awf_field-118411108-1" name="audience"  value="Men" tabindex="510" />
 <label class="choice" for="awf_field-118411108-1">Men</label>
 
 <div class="af-clear"></div></div><div class="af-element-radio multiChoice">
-<input class="radio" type="radio" id="awf_field-118411108-2" name="custom Target Audience"  value="Both" tabindex="511" />
+<input class="radio" type="radio" id="awf_field-118411108-2" name="audience"  value="Both" tabindex="511" />
 <label class="choice" for="awf_field-118411108-2">Both</label>
 
 <div class="af-clear"></div></div></div></div><div class="af-clear"></div></div>
 
 <div class="af-element"><div><label class="previewLabel" for="awf_field-118411109-0">Campaign Budget: [check one]</label><div class="choiceList choiceList-radio-stacked">
 <div class="af-element-radio multiChoice">
-<input class="radio" type="radio" id="awf_field-118411109-0" name="custom campaign budget"  value="$250 - $500" tabindex="512" />
+<input class="radio" type="radio" id="awf_field-118411109-0" name="budget"  value="$250 - $500" tabindex="512" />
 <label class="choice" for="awf_field-118411109-0">$250 &#8211; $500</label>
 
 <div class="af-clear"></div></div><div class="af-element-radio multiChoice">
-<input class="radio" type="radio" id="awf_field-118411109-1" name="custom campaign budget"  value="$500 - $1,500" tabindex="513" />
+<input class="radio" type="radio" id="awf_field-118411109-1" name="budget"  value="$500 - $1,500" tabindex="513" />
 <label class="choice" for="awf_field-118411109-1">$500 &#8211; $1,500</label>
 
 <div class="af-clear"></div></div><div class="af-element-radio multiChoice">
-<input class="radio" type="radio" id="awf_field-118411109-2" name="custom campaign budget"  value="$1,500 - $3,000" tabindex="514" />
+<input class="radio" type="radio" id="awf_field-118411109-2" name="budget"  value="$1,500 - $3,000" tabindex="514" />
 <label class="choice" for="awf_field-118411109-2">$1,500 &#8211; $3,000</label>
 
 <div class="af-clear"></div></div><div class="af-element-radio multiChoice">
-<input class="radio" type="radio" id="awf_field-118411109-3" name="custom campaign budget"  value="$3,000 and up" tabindex="515" />
+<input class="radio" type="radio" id="awf_field-118411109-3" name="budget"  value="$3,000 and up" tabindex="515" />
 <label class="choice" for="awf_field-118411109-3">$3,000 and up</label>
 
 <div class="af-clear"></div></div></div></div><div class="af-clear"></div></div>
